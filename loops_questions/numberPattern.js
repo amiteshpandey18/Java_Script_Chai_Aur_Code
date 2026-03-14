121deaa6fea9548104c8c8ed6dfc2486682df0d6
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-03-14 16:17:44 +0530

    message

---
+++ loops_questions/numberPattern.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 5; i++) {
+    let num = 1;
+    for (let j = 0; j < 5; j++) {
+        num=num+"";
+    }
+    //console.log(num);
+}
+    let age = Number(prompt("Enter your age"));
+    console.log(age);
+
+    
